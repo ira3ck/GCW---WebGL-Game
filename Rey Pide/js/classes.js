93ff9b509bc5131c7683035baf5a371673366dc3
MomoZV <moleconpollo1@gmail.com>
2021-04-18 04:08:44 -0500

    Movimiento y Recoger objetos basicos

---
+++ Rey Pide/js/classes.js
@@ -0,0 +1,15 @@
+class Pickable {
+    constructor(objeto, reco, seguirA, entregado) {
+      this.Objeto = objeto;
+      this.Recogido = reco;
+      this.Seguir = seguirA;
+      this.Entregado = entregado;
+    }
+    
+}
+
+class Player {
+    constructor(Puntaje){
+        
+    }
+}
